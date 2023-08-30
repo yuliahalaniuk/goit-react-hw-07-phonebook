8f75ffd8b49c44bb17b70bb04ccdef9bd5944493
yuliahalaniuk <122388855+yuliahalaniuk@users.noreply.github.com>
2023-08-30 18:15:04 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,4 @@
 export const getFilter = state => state.filter;
 export const getLoading = state => state.contacts.isLoading;
-export const getDeleting = state => state.contacts.isDeleting;
 export const getError = state => state.contacts.isError;
 export const getContacts = state => state.contacts.items;
